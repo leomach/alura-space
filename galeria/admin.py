e751219d425cd85fd96daf33d7e06273282916bd
Leandro Machado <leandrovictoram@gmail.com>
2024-05-28 08:11:12 -0300

    persistindo dados

---
+++ galeria/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from galeria.models import Fotografia
+
+class ListandoFotografia(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'legenda')
+    list_display_links = ('id', 'nome')
+
+admin.site.register(Fotografia, ListandoFotografia)
